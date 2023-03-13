74caa42b09c95a112183e725de57c6b3e8e26e24
br4wl0wl <ivan.d.mashkov@gmail.com>
2023-03-13 23:10:53 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t distcc . && docker run -t -d -p 3632:3632 -p 3633:3633 -p 5353:5353 distcc:latest
+docker build -t distcc . && docker run -p 3632:3632 -p 3633:3633 -p 5353:5353 -t -d distcc:latest
